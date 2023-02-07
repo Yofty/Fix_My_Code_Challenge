d4c6cfe27e22da03f50112558fea6aa19159a27b
Yofty <106748943+Yofty@users.noreply.github.com>
2023-02-07 14:47:48 -0800

    correction square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,28 +1,33 @@
 #!/usr/bin/python3
+import math
+"""calculate area nad perimeter of a square"""
+
 
 class square():
     
     width = 0
-    height = 0
 
     
     def __init__(self, *args, **kwargs):
+        """initiates square function"""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.height
+        return self.width * self.width
 
     def PermiterOfMySquare(self):
-        return (self.width * 2) + (self.height * 2)
+        """perimeter of square"""
+        return (self.width * 4)
 
     def __str__(self):
-        return "{}/{}".format(self.width, self.height)
+        """return given dimention"""
+        return "{}".format(self.width)
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=12)
+    s = square(width=12)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
